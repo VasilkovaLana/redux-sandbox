5da4addfe2e66d642d78194301c73aa6ae53013c
VasilkovaLana <45256974+VasilkovaLana@users.noreply.github.com>
2019-12-05 16:37:47 +0300

    redux-ui counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('Hello World!');
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'RND':
+      return state + action.payload;
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    default: 
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+document
+  .querySelector('.inc')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'INC'});
+  });
+
+document
+  .querySelector('.dec')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'DEC'});
+  });
+
+document
+  .querySelector('.rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random()*9);
+    store.dispatch({
+      type: 'RND',
+      payload
+    });
+  });
+
+const update = () => {
+  document
+    .querySelector('.counter')
+    .innerHTML = store.getState();
+};
+  
+store.subscribe(update);
+
