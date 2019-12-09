54d4c5c05afe951f11e03cadb410fdabda9a4176
VasilkovaLana <45256974+VasilkovaLana@users.noreply.github.com>
2019-12-09 15:04:56 +0300

    function connect

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
-import { createStore, bindActionCreators } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/app';
 import reducer from './reducer';
-import * as actions  from './actions';
 
 const store = createStore(reducer);
-const { dispatch } = store;
 
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
-document
-  .querySelector('.inc')
-  .addEventListener('click', inc);
 
-document
-  .querySelector('.dec')
-  .addEventListener('click', dec);
 
-document
-  .querySelector('.rnd')
-  .addEventListener('click', () => {
-    const payload = Math.floor(Math.random()*9);
-    rnd(payload);
-  });
-
-const update = () => {
-  document
-    .querySelector('.counter')
-    .innerHTML = store.getState();
-};
-  
-store.subscribe(update);
 
